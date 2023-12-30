6293eb52172724b9e27407de63e26bbfadb2522b
Noelphilip1990 <noyo1000gt@gmail.com>
2023-12-30 20:21:48 +0400

    list items from mongo db

--- server/index.js
+++ server/index.js
@@ -1,18 +1,38 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient } = require('mongodb');
+const dbUrl = "mongodb://localhost:27017";
 const app = express();
+
+const client = new MongoClient(dbUrl);
+
+
 
 
 // code exection
 
+async function main() {
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db('local');
+    const collection = db.collection('testlist');
+    const findResult = await collection.find({}).toArray();
+    return findResult;
+  }
+
 app.use(cors());
 
 app.get('/', (req, res) => {
-    const test = [
-        'test1', 'test2'
-    ]
-    
-    res.send(test);
+        main()
+        .then((result => {
+            console.log(result[0].data);
+            res.send(result[0].data);
+        }))
+        .catch(console.error)
+        .finally(() => client.close());
+
+        
 });
 
 
